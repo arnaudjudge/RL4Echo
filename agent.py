30093c458c68ed46e06e41d42cca58b1c7eb1aaf
arnaudjudge <arnaud.judge@usherbrooke.ca>
2023-07-07 15:19:11 -0400

    subtract baseline value from reward (seems to makes it work), add test step, optional sampling, fix batches, initial fill RB with sample from predicted distribution (instead of gt_mask)

--- agent.py
+++ agent.py
@@ -42,7 +42,7 @@ class Agent:
         self.replay_buffer = replay_buffer
         self.dataset = dataset
 
-    def get_action(self, img: np.array, prev_actions: np.array, net: nn.Module, epsilon: float, device: str) -> Tuple:
+    def get_action(self, img: np.array, prev_actions: np.array, net: nn.Module, epsilon: float, device: str, sample: bool = True) -> Tuple:
         """
         Using the given network, decide what action to carry out
         using an epsilon-greedy policy
@@ -61,23 +61,29 @@ class Agent:
         if len(image.shape) == 3:
             image = image.unsqueeze(0)
 
-        if type(prev_actions) == numpy.ndarray:
-            prev_actions = torch.from_numpy(prev_actions).cuda(device)
+        if prev_actions is not None:
+            if type(prev_actions) == numpy.ndarray:
+                prev_actions = torch.from_numpy(prev_actions).cuda(device)
+            if len(prev_actions.shape) == 3:
+                prev_actions = prev_actions.unsqueeze(1)
 
         if device not in ['cpu']:
             image = image.cuda(device)
 
         logits = torch.sigmoid(net(image))
-        # actions = torch.round(logits)
 
         distribution = Bernoulli(probs=logits)
         if prev_actions is None:
             prev_actions = distribution.sample()
-        actions = distribution.sample()
+
+        if sample:
+            actions = distribution.sample()
+        else:
+            actions = torch.round(logits)
         # random = torch.rand(actions.shape).to(device)
         # explored_actions = torch.where(random >= epsilon, actions, sample)
 
-        log_probs = distribution.log_prob(prev_actions).mean(dim=(2,3))
+        log_probs = distribution.log_prob(prev_actions)#.mean(dim=(2,3))
 
         return actions.squeeze(1), log_probs, logits.squeeze(1)
 
@@ -113,7 +119,7 @@ class Agent:
         # reward = r.squeeze(1).repeat(256, 256, 1).permute(2, 0, 1)
         # reward = torch.where(reward == 0, torch.tensor(-1).to(device), reward)
 
-        return simple.mean(dim=(1,2), keepdim=True)
+        return simple.mean(dim=(1,2), keepdim=True) - 0.75
 
     def play_step(self, buffer: ReplayBuffer, net: nn.Module, rewardnet: nn.Module, epsilon: float = 0.0, device: str = 'cuda:0') -> Tuple[float, bool]:
         """
@@ -129,7 +135,7 @@ class Agent:
 
         img, gt_mask = self.dataset.get_new_image()
 
-        action, log_prob, seg = self.get_action(img, gt_mask, net, epsilon, device)
+        action, log_prob, seg = self.get_action(img, None, net, epsilon, device)
 
         # get reward
         reward = self.get_reward(img, seg, rewardnet, gt_mask, device)
