fb718f53509aa04eab1f1e77e4480455286dcef8
arnaudjudge <arnaud.judge@usherbrooke.ca>
2023-12-12 15:47:38 -0500

    refactor datamodule to use dataframe's split column

--- SectorDataModule.py
+++ SectorDataModule.py
@@ -6,29 +6,23 @@ import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 import torch
+from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
 from torch.utils.data import random_split, DataLoader
 
 
 class SectorDataset(Dataset):
-    def __init__(self, data_path, csv_file, subset_frac=1.0, test_frac=0.1, available_gt=1.0, seed=0, test=False, *args,
+    def __init__(self, df, data_path, subset_frac=1.0, available_gt=1.0, seed=0, test=False, *args,
                  **kwargs):
         super().__init__()
+        self.df = df
         self.data_path = data_path
+        self.test = test
 
-        self.df = pd.read_csv(csv_file, index_col=0)
-        self.df = self.df[(self.df['passed'] == True) & (self.df['relative_path'].notna())]
+        # only use subset
+        self.df = self.df.sample(n=int(subset_frac * len(self.df)), random_state=seed)
 
-        # split according to test_frac
-        test_len = int(test_frac * len(self.df))
-        if test:
-            self.df = self.df.iloc[-test_len:]
-        else:
-            self.df = self.df.iloc[:-test_len]
-            # only use subset
-            self.df = self.df.sample(n=int(subset_frac * len(self.df)), random_state=seed)
-
-        print(f"Test step: {test} , len of dataset {len(self.df)}")
+        print(f"Test step: {self.test} , len of dataset {len(self.df)}")
 
         # create list of available ground truths
         self.use_gt = np.zeros(len(self.df))
@@ -55,11 +49,19 @@ class SectorDataModule(pl.LightningDataModule):
     DataModule used for semantic segmentation in geometric generalization project
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, data_dir, csv_file, splits_column='split_0', subset_frac=1.0, test_frac=0.1, available_gt=1.0, seed=0, *args, **kwargs):
         super().__init__()
         self.args = args
         self.kwargs = kwargs
 
+        self.save_hyperparameters(logger=False)
+
+        # open dataframe for dataset
+        self.df = pd.read_csv(self.hparams.data_dir + '/' + self.hparams.csv_file, index_col=0)
+
+        self.train: Optional[torch.utils.Dataset] = None
+        self.validate: Optional[torch.utils.Dataset] = None
+        self.test: Optional[torch.utils.Dataset] = None
     def prepare_data(self):
         """
         Empty prepare_data method left in intentionally.
@@ -79,15 +81,49 @@ class SectorDataModule(pl.LightningDataModule):
         # Assign train/val datasets for use in dataloaders
         # the stage is used in the Pytorch Lightning trainer method, which you can call as fit (training, evaluation) or test, also you can use it for predict, not implemented here
 
+        self.df = self.df[(self.df['passed'] == True) & (self.df['relative_path'].notna())]
+
+        # Do splits
+        if self.hparams.splits_column and self.hparams.splits_column in self.df.columns:
+            # splits are already defined in csv file
+            print(f"Using split from column: {self.hparams.splits_column}")
+            self.train_idx = self.df.index[self.df[self.hparams.splits_column] == 'train'].tolist()
+            self.val_idx = self.df.index[self.df[self.hparams.splits_column] == 'val'].tolist()
+            self.test_idx = self.df.index[self.df[self.hparams.splits_column] == 'test'].tolist()
+        else:
+            # create new splits, save if column name is given
+            print(f"Creating new splits!")
+            self.train_idx, val_and_test_idx = train_test_split(self.df.index.to_list(),
+                                                                train_size=0.8,
+                                                                random_state=self.hparams.seed)
+            self.val_idx, self.test_idx = train_test_split(val_and_test_idx,
+                                                           test_size=0.5,
+                                                           random_state=self.hparams.seed)
+            if self.hparams.splits_column:
+                print(f"Saving new split to column: {self.hparams.splits_column}")
+                self.df.loc[self.train_idx, self.hparams.splits_column] = 'train'
+                self.df.loc[self.val_idx, self.hparams.splits_column] = 'val'
+                self.df.loc[self.test_idx, self.hparams.splits_column] = 'test'
+                self.df.to_csv(self.hparams.data_dir + '/' + self.hparams.csv_file)
+
         if stage == "fit" or stage is None:
-            train_set_full = SectorDataset(*self.args, **self.kwargs)
-            train_set_size = int(len(train_set_full) * 0.9)
-            valid_set_size = len(train_set_full) - train_set_size
-            self.train, self.validate = random_split(train_set_full, [train_set_size, valid_set_size])
+            self.train = SectorDataset(self.df.loc[self.train_idx],
+                                           data_path=self.hparams.data_dir,
+                                           subset_frac=self.hparams.subset_frac,
+                                           seed=self.hparams.seed)
+
+            self.validate = SectorDataset(self.df.loc[self.val_idx],
+                                          data_path=self.hparams.data_dir,
+                                          subset_frac=self.hparams.subset_frac,
+                                          seed=self.hparams.seed)
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
-            self.test = SectorDataset(*self.args, **self.kwargs, test=True)
+            self.test = SectorDataset(self.df.loc[self.val_idx],
+                                          data_path=self.hparams.data_dir,
+                                          subset_frac=self.hparams.subset_frac,
+                                          seed=self.hparams.seed,
+                                          test=True)
 
     # define your dataloaders
     # again, here defined for train, validate and test, not for predict as the project is not there yet.
@@ -102,10 +138,10 @@ class SectorDataModule(pl.LightningDataModule):
 
 
 if __name__ == "__main__":
-    dl = SectorDataModule('/home/local/USHERBROOKE/juda2901/dev/data/icardio/train_subset_1k/',
-                          '/home/local/USHERBROOKE/juda2901/dev/data/icardio/train_subset_1k/subset.csv',
-                          0.1,
-                          0.1)
+    dl = SectorDataModule(data_dir='/home/local/USHERBROOKE/juda2901/dev/data/icardio/train_subset_10k/',
+                          csv_file='subset.csv',
+                          subset_frac=0.1,
+                          test_frac=0.1)
 
     dl.setup()
     for i in range(1):
