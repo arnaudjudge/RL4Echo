23ba8e9f249c37876af18b5af685ab5bec0edb2c
arnaudjudge <arnaud.judge@usherbrooke.ca>
2024-02-16 08:31:06 -0500

    update scripts and configs

--- scripts/splits_from_qc.py
+++ scripts/splits_from_qc.py
@@ -3,14 +3,17 @@ import pandas as pd
 
 if __name__=="__main__":
 
-    qc_1 = '/home/local/USHERBROOKE/juda2901/dev/data/icardio/20240208_olivier_icardio_es_ed_testset.json'
-    qc_2 = '/home/local/USHERBROOKE/juda2901/dev/data/icardio/20240209_NDUCH_icardio_es_ed_testset.json'
+    #qc_1 = '/home/local/USHERBROOKE/juda2901/dev/data/icardio/20240214_olivier_icardio_es_ed_testset2.0.json'
+    #qc_2 = '/home/local/USHERBROOKE/juda2901/dev/data/icardio/20240215_NDUCH_icardio_es_ed_testset2.0.json'
 
-    df_path = '/home/local/USHERBROOKE/juda2901/dev/data/icardio/ES_ED_train_subset_2/subset_INITIAL.csv'
+    qc_1 = '/home/local/USHERBROOKE/juda2901/dev/data/icardio/20240215_olivier_icardio_es_ed_testset3.0.json'
+    qc_2 = '/home/local/USHERBROOKE/juda2901/dev/data/icardio/20240215_NDUCH_icardio_es_ed_testset3.0.json'
+
+    df_path = '/home/local/USHERBROOKE/juda2901/dev/data/icardio/ES_ED_train_subset_affine/subset_official_test_second.csv'
     df = pd.read_csv(df_path, index_col=0)
 
-    df['split_0'] = 'pred'
-
+    df.loc[df['split_0'] != 'test', 'split_0'] = 'pred'
+    img_dict = {}
     with open(qc_1, "r") as f1:
         with open(qc_2, "r") as f2:
             data1 = json.load(f1)
@@ -27,11 +30,19 @@ if __name__=="__main__":
                     count += 1
                     dicom = img1['filename'].split("_")[0]
                     inst = img1['filename'].split("_")[1]
-
-                    df.loc[(df['dicom_uuid'] == dicom) & (df['instant'] == inst), 'split_0'] = 'test'
+                    img_dict[dicom] = img_dict.get(dicom, []) + [inst]
 
             print(count)
+    count = 0
+    for dicom, i in img_dict.items():
+        if len(i) < 2:
+            continue
+        count += 1
+        for inst in i:
+            df.loc[(df['dicom_uuid'] == dicom) & (df['instant'] == inst), 'split_0'] = 'test'
+    print(f"New patients in test: {count}")
     df.loc[df[df['split_0'] == 'pred'].sample(n=100).index, 'split_0'] = 'val'  # val set
     df.loc[df[df['split_0'] == 'pred'].sample(n=100).index, 'split_0'] = 'train'  # initial train
 
-    df.to_csv(df_path.replace('_INITIAL.csv', '_official_test.csv'))
+    print(df[df['split_0'] == 'test'])
+    df.to_csv(df_path.replace('_second.csv', '.csv'))
