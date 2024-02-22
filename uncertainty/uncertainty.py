d6cee0cb32375311156d6715e8e424a7599b07e6
thierry judge <tejudge@gmail.com>
2024-02-22 19:35:58 +0100

    add initial uncertainty files

---
+++ uncertainty/uncertainty.py
@@ -0,0 +1,87 @@
+from typing import Any, Dict, Mapping, Sequence, Union
+
+import pytorch_lightning as pl
+import torch
+from torch import Tensor
+from torch.nn import functional as F
+
+from uncertainty.unet import UNet
+from vital.metrics.train.metric import DifferentiableDiceCoefficient
+from vital.data.camus.config import Label
+
+def prefix(map: Mapping[str, Any], prefix: str, exclude: Union[str, Sequence[str]] = None) -> Dict[str, Any]:
+    """Prepends a prefix to the keys of a mapping with string keys.
+
+    Args:
+        map: Mapping with string keys for which to add a prefix to the keys.
+        prefix: Prefix to add to the current keys in the mapping.
+        exclude: Keys to exclude from the prefix addition. These will remain unchanged in the new mapping.
+
+    Returns:
+        Mapping where the keys have been prepended with `prefix`.
+    """
+    if exclude is None:
+        exclude = []
+    elif isinstance(exclude, str):
+        exclude = [exclude]
+
+    return {f"{prefix}{k}" if k not in exclude else k: v for k, v in map.items()}
+
+
+class SegmentationUncertainty(pl.LightningModule):
+    def __init__(self, input_shape=(1, 256, 256), output_shape=(3, 256, 256), *args, **kwargs):
+        super().__init__(**kwargs)
+        self.save_hyperparameters()
+
+        self.net = UNet(input_shape=input_shape, output_shape=output_shape)
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+        self._dice = DifferentiableDiceCoefficient(include_background=False, reduction="none")
+
+        self.hparams.ce_weight = 0.1
+        self.hparams.dice_weight = 1
+
+
+    def forward(self, x):
+        return self.net.forward(x)
+
+    def configure_optimizers(self):
+        # add weight decay so predictions are less certain, more randomness?
+        return torch.optim.Adam(self.parameters(), lr=0.001, weight_decay=1e-4)
+
+    def _shared_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Dict[str, Tensor]:  # noqa: D102
+        x, y = batch['img'], batch['gt']
+
+        # Forward
+        y_hat = self.net(x)
+
+        # Segmentation accuracy metrics
+        if y_hat.shape[1] == 1:
+            ce = F.binary_cross_entropy_with_logits(y_hat.squeeze(), y.type_as(y_hat))
+        else:
+            ce = F.cross_entropy(y_hat, y)
+
+        dice_values = self._dice(y_hat, y)
+        dices = {f"dice/{label}": dice for label, dice in zip([Label.LV, Label.MYO], dice_values)}
+        mean_dice = dice_values.mean()
+
+        loss = (self.hparams.ce_weight * ce) + (self.hparams.dice_weight * (1 - mean_dice))
+
+        # Format output
+        return {"loss": loss, "ce": ce, "dice": mean_dice, **dices}
+
+    def training_step(self, *args, **kwargs) -> Dict[str, Tensor]:  # noqa: D102
+        result = prefix(self._shared_step(*args, **kwargs), "train/")
+        self.log_dict(result, prog_bar=False, logger=True, on_step=True, on_epoch=None)
+        # Add reference to 'train_loss' under 'loss' keyword, requested by PL to know which metric to optimize
+        result["loss"] = result["train/loss"]
+        return result
+
+    def validation_step(self, *args, **kwargs) -> Dict[str, Tensor]:  # noqa: D102
+        result = prefix(self._shared_step(*args, **kwargs), "val/")
+        self.log_dict(result, prog_bar=False, logger=True, on_step=None, on_epoch=True)
+        return result
+
+    def test_step(self, batch, batch_idx):
+        pass
