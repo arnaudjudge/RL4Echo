5d0ace56db9799bee6f12e4bc7f13a11731ec82a
arnaudjudge <arnaud.judge@usherbrooke.ca>
2024-03-11 05:56:08 -0400

    clean up

--- auto_iteration.py
+++ auto_iteration.py
@@ -12,7 +12,9 @@ from omegaconf import OmegaConf
 from runner import main as runner_main
 
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 @hydra.main(version_base=None, config_path="config", config_name="auto_iteration")
 def main(cfg):
@@ -21,7 +23,7 @@ def main(cfg):
 
     iterations = cfg.num_iter
     output_path = cfg.output_path
-    Path(output_path+"/0/").mkdir(parents=True, exist_ok=True)
+    Path(output_path + "/0/").mkdir(parents=True, exist_ok=True)
     main_overrides = [f"logger.save_dir={output_path}"]
     target_experiment = cfg.target
     source_experiment = cfg.source
@@ -32,7 +34,6 @@ def main(cfg):
 
     # train supervised network for initial actor
     overrides = main_overrides + [f"trainer.max_epochs={cfg.sup_num_epochs}",
-                                  #"datamodule.subset_frac=0.0075",
                                   f'model.predict_save_dir={None}',  # no predictions here
                                   f"model.ckpt_path={output_path}/{0}/actor.ckpt",
                                   f"model.loss.label_smoothing={cfg.sup_loss_label_smoothing}",
@@ -54,8 +55,8 @@ def main(cfg):
     overrides = main_overrides + cfg.rl_overrides + [f"trainer.max_epochs=0",
                                                      f"predict_subset_frac={cfg.rl_num_predict}",
                                                      f"model.actor.actor.pretrain_ckpt={output_path}/{0}/actor.ckpt",
-                                                     f"model.actor.actor.ref_ckpt={output_path}/{0}/actor.ckpt",  # always supervised
-                                                     "reward@model.reward=pixelwise_accuracy",  # dummy, will not be used
+                                                     f"model.actor.actor.ref_ckpt={output_path}/{0}/actor.ckpt",
+                                                     "reward@model.reward=pixelwise_accuracy",  # will not be used
                                                      f"model.actor_save_path=null",  # no need
                                                      f"model.critic_save_path=null",  # no need
                                                      f'model.predict_save_dir={output_path}',
@@ -72,11 +73,11 @@ def main(cfg):
     sub_cfg.datamodule.gt_column = experiment_gt_column
     runner_main(sub_cfg)
 
-    for i in range(1, iterations+1):
+    for i in range(1, iterations + 1):
         # train reward net
         overrides = main_overrides + [f"trainer.max_epochs={cfg.rn_num_epochs}",
                                       f"datamodule.data_path={output_path}/",
-                                      f"model.save_model_path={output_path}/{i-1}/rewardnet.ckpt",
+                                      f"model.save_model_path={output_path}/{i - 1}/rewardnet.ckpt",
                                       f"+model.var_file={cfg.var_file}"]
         sub_cfg = compose(config_name=f"reward_runner.yaml", overrides=overrides)
         print(OmegaConf.to_yaml(sub_cfg))
@@ -90,24 +91,25 @@ def main(cfg):
         saved_vars = pickle.load(open(cfg.var_file, "rb"))
 
         # train PPO model with fresh reward net
-        overrides = main_overrides + cfg.rl_overrides + [f"trainer.max_epochs={cfg.rl_num_epochs}",
-                                                         f"predict_subset_frac={cfg.rl_num_predict}",
-                                                         f"datamodule.splits_column={experiment_split_column}",
-                                                         f"datamodule.gt_column={experiment_gt_column}",
-                                                         f"+datamodule.train_batch_size={8*i}", #current is 8*i
-                                                         f"model.actor.actor.pretrain_ckpt={output_path}/{i-1}/actor.ckpt",
-                                                         f"model.actor.actor.ref_ckpt={output_path}/{0}/actor.ckpt", # always supervised?
-                                                         f"model.reward.state_dict_path={output_path}/{i-1}/rewardnet.ckpt",
-                                                         #f"model.reward.temp_factor={float(saved_vars['Temperature_factor'])}",
-                                                         f"model.actor_save_path={output_path}/{i}/actor.ckpt",
-                                                         f"model.critic_save_path={output_path}/{i}/critic.ckpt",
-                                                         f'model.predict_save_dir={output_path if iterations > i else None}',
-                                                         f"model.entropy_coeff={max(0.3 / (i*2), 0)}",
-                                                         f"model.divergence_coeff={0.1 / (i*2)}",
-                                                         f"experiment=ppo_{target_experiment}"
-                                                         ]
-        if Path(f"{output_path}/{i-1}/critic.ckpt").exists():
-            overrides += [f"model.actor.critic.pretrain_ckpt={output_path}/{i-1}/critic.ckpt"]
+        overrides = main_overrides + cfg.rl_overrides + \
+                    [f"trainer.max_epochs={cfg.rl_num_epochs}",
+                     f"predict_subset_frac={cfg.rl_num_predict}",
+                     f"datamodule.splits_column={experiment_split_column}",
+                     f"datamodule.gt_column={experiment_gt_column}",
+                     f"+datamodule.train_batch_size={8 * i}",
+                     f"model.actor.actor.pretrain_ckpt={output_path}/{i - 1}/actor.ckpt",
+                     f"model.actor.actor.ref_ckpt={output_path}/{0}/actor.ckpt",  # always supervised
+                     f"model.reward.state_dict_path={output_path}/{i - 1}/rewardnet.ckpt",
+                     # f"model.reward.temp_factor={float(saved_vars['Temperature_factor'])}",
+                     f"model.actor_save_path={output_path}/{i}/actor.ckpt",
+                     f"model.critic_save_path={output_path}/{i}/critic.ckpt",
+                     f'model.predict_save_dir={output_path if iterations > i else None}',
+                     f"model.entropy_coeff={max(0.3 / (i * 2), 0)}",
+                     f"model.divergence_coeff={0.1 / (i * 2)}",
+                     f"experiment=ppo_{target_experiment}"
+                     ]
+        if Path(f"{output_path}/{i - 1}/critic.ckpt").exists():
+            overrides += [f"model.actor.critic.pretrain_ckpt={output_path}/{i - 1}/critic.ckpt"]
         sub_cfg = compose(config_name=f"RL_runner.yaml", overrides=overrides)
         print(OmegaConf.to_yaml(sub_cfg))
         runner_main(sub_cfg)
