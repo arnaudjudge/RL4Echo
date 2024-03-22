7c197f3988bf497b0ddbe9c56989099b1534028a
arnaudjudge <arnaud.judge@usherbrooke.ca>
2024-03-22 05:22:07 -0400

    inference with actor version 2

--- rl4echo/inference_from_seq_batch.py
+++ rl4echo/inference_from_seq_batch.py
@@ -18,7 +18,7 @@ CSV_PATH = "/data/icardio/MICCAI2024/MICCAI2024.csv"
 IMG_PATH = "/data/icardio/processed/img/"
 
 OUT_PATH = "/data/icardio/MICCAI2024/segmentation/"
-CAP = 1000
+CAP = 10000
 
 if __name__ == "__main__":
 
@@ -26,8 +26,6 @@ if __name__ == "__main__":
     df = df[df['passed'] == True]
     df['MICCAI2024_frame_AV'] = df.get("MICCAI2024_frame_AV", None)
     df['MICCAI2024_overall_AV'] = df.get("MICCAI2024_overall_AV", None)
-
-    resize_down = Resize((256, 256, 1))
 
     model = UNet(input_shape=(1, 256, 256), output_shape=(3, 256, 256))
     model.load_state_dict(torch.load("/data/rl_logs/run_2_batchsize_increase/4/actor.ckpt"))
@@ -38,8 +36,8 @@ if __name__ == "__main__":
             model.cuda()
             img = img.cuda()
         out = torch.sigmoid(model(img))
-        return out.cpu().detach().numpy()[0]
-    j = 0
+        return out.cpu().detach().numpy()
+    j = 1
 
     for idx in tqdm(df[df['MICCAI2024_overall_AV'].isna()].index, total=CAP):
         row = df.loc[idx].to_dict()
@@ -50,41 +48,35 @@ if __name__ == "__main__":
         data = nifti_img.get_fdata()
 
         data = data / 256
-        resize_up = Resize((data.shape[0], data.shape[1], 1))
+        resize_down = Resize((256, 256, data.shape[-1]))
+        resize_up = Resize(data.shape)
         label_seq = np.zeros_like(data, dtype=np.uint32)
 
-        for i in range(data.shape[-1]):
-            d = data[..., i]
-            d = exp.equalize_adapthist(d, clip_limit=0.01)
-            d_small = resize_down(ScalarImage(tensor=d[None, ..., None], affine=nifti_img.affine))
+        data = exp.equalize_adapthist(data, clip_limit=0.01)
+        d_small = resize_down(ScalarImage(tensor=data[None, ...], affine=nifti_img.affine))
 
-            # segment and post-process
-            s = get_segmentation(d_small.numpy().transpose((0, 3, 1, 2)))
-            s = np.argmax(s, axis=0)
-            lbl, num = ndimage.label(s != 0)
+        # segment and post-process
+        s = get_segmentation(d_small.numpy().transpose((3, 0, 1, 2)))
+        s = np.argmax(s, axis=1)
+        for i in range(len(s)):
+            lbl, num = ndimage.label(s[i] != 0)
             # Count the number of elements per label
             count = np.bincount(lbl.flat)
             # Select the largest blob
             maxi = np.argmax(count[1:]) + 1
             # Remove the other blobs
-            s[lbl != maxi] = 0
+            s[i][lbl != maxi] = 0
 
-            label = LabelMap(tensor=s[None, ..., None], affine=d_small.affine)
-            label = resize_up(label)
+        label = LabelMap(tensor=s.transpose((1, 2, 0))[None, ...], affine=d_small.affine)
+        label = resize_up(label).numpy()[0]
 
-            # plt.figure()
-            # plt.imshow(d.T, cmap='gray')
-            # plt.imshow(label.numpy()[0, ..., 0].T, alpha=0.3)
-            # plt.show()
-            label_seq[..., i] = label.numpy()[0, ..., 0]
-
-        label_nifti = nib.Nifti1Image(label_seq, header=nib.Nifti1Header(), affine=nifti_img.affine)
+        label_nifti = nib.Nifti1Image(label, header=nib.Nifti1Header(), affine=nifti_img.affine)
         label_path = Path(OUT_PATH) / row['study'] / row['view'].lower() / f"{row['dicom_uuid']}.nii.gz"
         label_path.parent.mkdir(parents=True, exist_ok=True)
         nib.save(label_nifti, label_path)
 
         # anatomical metrics
-        anatomical_validities = is_anatomically_valid(label_seq.transpose(2, 0, 1))
+        anatomical_validities = is_anatomically_valid(label.transpose(2, 0, 1))
 
         row['MICCAI2024_frame_AV'] = anatomical_validities
         row['MICCAI2024_overall_AV'] = all(anatomical_validities)
@@ -92,5 +84,5 @@ if __name__ == "__main__":
         df.to_csv(CSV_PATH)
 
         j += 1
-        if j > CAP:
+        if j >= CAP:
             break
