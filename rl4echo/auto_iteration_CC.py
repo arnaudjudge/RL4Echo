b3f94f8e899f2e5953ac65b6a464dab762decb7a
arnaudjudge <arnaud.judge@usherbrooke.ca>
2024-08-28 14:03:50 -0400

    changes to autoiteration for CC

--- rl4echo/auto_iteration_CC.py
+++ rl4echo/auto_iteration_CC.py
@@ -2,6 +2,7 @@ import pickle
 from datetime import datetime
 from pathlib import Path
 
+import os
 import hydra
 import pandas as pd
 from dotenv import load_dotenv
@@ -52,15 +53,17 @@ def main(cfg):
         sub_cfg.datamodule.gt_column = experiment_gt_column
         runner_main(sub_cfg)
 
+    cc_overrides = ["+launcher=narval", "hydra.launcher.timeout_min=15"]
+
     # Predict and test (baseline) on target domain
-    overrides = main_overrides + cfg.rl_overrides + [f"trainer.max_epochs=0",
+    overrides = main_overrides + cfg.rl_overrides + cc_overrides + [f"trainer.max_epochs=0",
                                                      f"predict_subset_frac={cfg.rl_num_predict}",
                                                      f"model.actor.actor.pretrain_ckpt={f'{output_path}/{0}/actor.ckpt' if pretrain_path is None else pretrain_path}",
                                                      f"model.actor.actor.ref_ckpt={f'{output_path}/{0}/actor.ckpt' if pretrain_path is None else pretrain_path}",
                                                      "reward@model.reward=pixelwise_accuracy",  # will not be used
                                                      f"model.actor_save_path={output_path}/{0}/actor.ckpt",  # no need
                                                      f"model.critic_save_path=null",  # no need
-                                                     f'model.predict_save_dir={output_path}',
+                                                     f"model.predict_save_dir={output_path}/rewardDS/",
                                                      f"experiment=ppo_{target_experiment}"
                                                      ]
     sub_cfg = compose(config_name=f"RL_3d_runner.yaml", overrides=overrides)
@@ -75,11 +78,14 @@ def main(cfg):
     runner_main(sub_cfg)
 
     for i in range(1, iterations + 1):
+        # set OS data path for copy of data to happen
+        os.environ["DATA_PATH"] = f'{output_path}/rewardDS/'
         # train reward net
-        overrides = main_overrides + [f"trainer.max_epochs={cfg.rn_num_epochs}",
-                                      f"datamodule.data_path={output_path}/",
+        overrides = main_overrides + cc_overrides + [f"trainer.max_epochs={cfg.rn_num_epochs}",
+                                      f"datamodule.data_path={output_path}/rewardDS/",
                                       f"model.save_model_path={output_path}/{i - 1}/rewardnet.ckpt",
-                                      f"+model.var_file={cfg.var_file}"]
+                                      ]
+                                      # f"+model.var_file={cfg.var_file}"]
         sub_cfg = compose(config_name=f"reward_3d_runner.yaml", overrides=overrides)
         print(OmegaConf.to_yaml(sub_cfg))
         runner_main(sub_cfg)
@@ -89,14 +95,15 @@ def main(cfg):
 
         # TODO: MAYBE RETHINK THIS WAY OF PASSING THE VARIABLE(S)
         # load temporary variable file
-        saved_vars = pickle.load(open(cfg.var_file, "rb"))
+        # saved_vars = pickle.load(open(cfg.var_file, "rb"))
 
+        load_dotenv()
         # train PPO model with fresh reward net
-        overrides = main_overrides + cfg.rl_overrides + \
+        overrides = main_overrides + cfg.rl_overrides + cc_overrides + \
                     [f"trainer.max_epochs={cfg.rl_num_epochs}",
                      f"predict_subset_frac={cfg.rl_num_predict}",
                      f"datamodule.splits_column={experiment_split_column}",
-                     f"datamodule.gt_column={experiment_gt_column}",
+                     f"datamodule.gt_column=null", #{experiment_gt_column}",
                      f"+datamodule.train_batch_size={8 * i}",
                      f"model.actor.actor.pretrain_ckpt={output_path}/{i - 1}/actor.ckpt",
                      f"model.actor.actor.ref_ckpt={output_path}/{0}/actor.ckpt",  # always supervised
@@ -104,7 +111,7 @@ def main(cfg):
                      # f"model.reward.temp_factor={float(saved_vars['Temperature_factor'])}",
                      f"model.actor_save_path={output_path}/{i}/actor.ckpt",
                      f"model.critic_save_path={output_path}/{i}/critic.ckpt",
-                     f'model.predict_save_dir={output_path if iterations > i else None}',
+                     f'model.predict_save_dir={f"{output_path}/rewardDS/" if iterations > i else None}',
                      f"model.entropy_coeff={max(0.3 / (i * 2), 0)}",
                      f"model.divergence_coeff={0.1 / (i * 2)}",
                      f"experiment=ppo_{target_experiment}"
