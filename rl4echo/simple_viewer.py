ce7dc8b2666e79c3f7055038dbb40628cfc6a0a6
arnaudjudge <ahjudge@gmail.com>
2025-10-20 16:33:42 -0400

    create simple utility for basic viewing with matplotlib

---
+++ rl4echo/simple_viewer.py
@@ -0,0 +1,69 @@
+import matplotlib
+matplotlib.use('TKAgg')
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import argparse
+import SimpleITK as sitk
+import pydicom
+import os
+
+
+def load_image(path):
+    """
+    Load a single medical image (NIfTI or DICOM).
+
+    Args:
+        path (str): Path to a single .nii/.nii.gz or .dcm file.
+
+    Returns:
+        tuple: (image_array, spacing)
+            image_array (np.ndarray): Image data.
+            spacing (tuple): Pixel spacing or voxel spacing.
+    """
+    ext = os.path.splitext(path)[-1].lower()
+
+    if ext in [".nii", ".gz"]:  # NIfTI
+        img = sitk.ReadImage(path)
+        arr = sitk.GetArrayFromImage(img)
+        return arr
+    elif ext == ".dcm":  # Single DICOM file
+        dcm = pydicom.dcmread(path)
+        arr = dcm.pixel_array
+        if len(arr.shape) > 3:
+            arr = arr.mean(-1)
+        return arr
+
+    else:
+        raise ValueError(f"Unsupported file type: {ext}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Load a single NIfTI or DICOM image.")
+    parser.add_argument(
+        "input", type=str, help="Path to a single .nii/.nii.gz or .dcm file.")
+    parser.add_argument(
+        "--save", "-s", type=str, required=False, help="Path to a .gif file.")
+    args = parser.parse_args()
+
+    arr = load_image(args.input)
+    print(f"Loaded image with shape {arr.shape}")
+
+    f, ax = plt.subplots()
+    im = ax.imshow(arr[0, ...], animated=True, cmap='gray')
+    def update(i):
+        im.set_array(arr[i, ...])
+        return im,
+
+    animation_fig = animation.FuncAnimation(f, update, frames=arr.shape[0], interval=100, blit=True, repeat_delay=10)
+
+    if args.save:
+        animation_fig.save(args.save)
+        print(f"Saved gif to {args.save}")
+        plt.close()
+    else:
+        plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
