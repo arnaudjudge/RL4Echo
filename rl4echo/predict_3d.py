e6bbf213ca92e9d8278ee58ba554e022b66151c6
arnaudjudge <ahjudge@gmail.com>
2025-10-21 20:26:44 -0400

    fix strip issue

--- rl4echo/predict_3d.py
+++ rl4echo/predict_3d.py
@@ -111,7 +111,7 @@ class RL4Echo3DPredictor:
         # Handle single NIfTI file
         if input_path.is_file() and input_path.suffix == ".txt":
             with open(input_path) as f:
-                nifti_files = [Path(line.strip()) for line in f if line.strip()]
+                nifti_files = [Path(line.removesuffix("_0000")) for line in f if line.removesuffix("_0000")]
         elif input_path.is_file() and (input_path.suffix == ".nii" or "".join(input_path.suffixes[-2:]) == ".nii.gz"):
             nifti_files = [input_path]
         # Handle folder of NIfTI files
