740b0e9f540ca69d86d16179f3f3e2de5d4324b6
Arnaud Judge <55412727+arnaudjudge@users.noreply.github.com>
2025-11-04 13:33:50 -0500

    Refactor reward merging and saving logic

--- rl4echo/RLmodule_3D.py
+++ rl4echo/RLmodule_3D.py
@@ -790,12 +790,11 @@ class RLmodule3D(LightningModule):
         self.save_mask(croporpad(transform(tio.LabelMap(tensor=preds, affine=resampled_affine))).numpy()[0],
                        fname, spacing, save_dir)
 
-        all_merged = np.stack([rew[0], rew[1], merged], axis=0)
-        self.save_mask(croporpad(transform(tio.ScalarImage(tensor=all_merged, affine=resampled_affine))).numpy()[0]*255,
-                       fname + "_merged_all_rewards_reward", spacing, save_dir)
-        #[self.save_mask(croporpad(transform(tio.ScalarImage(tensor=rew[i], affine=resampled_affine))).numpy()[0]*255,
-        #               fname + f"_{i}_reward", spacing, save_dir) for i in range(len(rew))]
-
+        merged_resized = croporpad(transform(tio.ScalarImage(tensor=all_merged, affine=resampled_affine))).numpy()[0]*255
+        rew_resized = [croporpad(transform(tio.ScalarImage(tensor=rew[i], affine=resampled_affine))).numpy()[0]*255 for i in range(len(rew))]
+        all_merged = np.stack([rew_resized[0], rew_resized[1], merged_resized], axis=0)
+        self.save_mask(all_merged, fname + "_merged_all_rewards_reward", spacing, save_dir)
+        
         csvfilename = properties_dict.get("csv_filename")[0]
         header = ['dicom_uuid', 'anat_val', 'anat_val_frames', 'temporal_val', 'temporal_errors', 'min_reward_frame', 'tto']
 
